e8ed0e564e9ca41c2ce630eb57c8300cf39b468d
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2018-03-31 23:21:14 +0530

    Refactor code

--- cli.js
+++ cli.js
@@ -14,16 +14,27 @@ const prompt = new Prompt({
 	name: 'pass'
 });
 
+let isCopied = false; // For clipboard checking
+
 /**
- * Check & clear password from the clipboard if there is any
+ * Check clipboard if the given password was copied
  * @param {string} pass - The password to check
  */
-const clearClipboard = pass => {
-	if (clipboardy.readSync() === pass) {
-		console.log();
-		console.log(`${logSymbols.info} clearing clipboard...`);
-		clipboardy.writeSync('😇  Thanks for using a strong password️');
+const checkClipboard = pass => {
+	return clipboardy.readSync() === pass;
+};
+
+/**
+ * Clear clipboard util
+ * @param {string} res - Result from the pwned password
+ */
+const clearClipboard = res => {
+	if (!isCopied) {
+		return;
 	}
+	console.log(`\n${logSymbols.info} clearing clipboard...`);
+	// Appreciate :)
+	clipboardy.writeSync(res.pwned ? '' : '😇  Thanks for using a strong password');
 };
 
 /**
@@ -31,26 +42,31 @@ const clearClipboard = pass => {
  * @param {Object} res - The password to check
  * @param {Object} res - The password to check
  */
-const print = (res, pass) => {
+const showResult = res => {
 	const messages = {
 		pwned: `${logSymbols.error} Pwned ${chalk.yellow(res.count)} times!`,
 		safe: `${logSymbols.success} Yay! No records found!`
 	};
-
 	console.log();
 	if (res.pwned) {
 		console.log(cowsay.think({text: messages.pwned, e: 'oO'}));
+		// ShowWarning();
 	} else {
 		console.log(cowsay.say({text: messages.safe, f: 'tux'}));
 	}
-	clearClipboard(pass);
+	return res;
 };
 
-prompt.run().then(pass => {
+const checkPass = pass => {
 	if (!pass) {
 		console.error(`${logSymbols.warning} Specify a password`);
 		process.exit(1);
 	}
-	passw0rd.check(pass)
-		.then(res => print(res, pass));
-});
+	isCopied = checkClipboard(pass);
+	return passw0rd.check(pass);
+};
+
+prompt.run()
+	.then(checkPass)
+	.then(showResult)
+	.then(clearClipboard);
